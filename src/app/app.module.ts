68cc815ddefa498e9d2af2974ddf1804c4ddbdee
Mark DeRose <mderose25@gmail.com>
2018-10-16 20:56:57 -0500

    9pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,13 @@ import { firebaseConfig } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { LoginService } from './login/login.service';
 import { AppRoutingModule } from './app-routing.module';
 
 import {AuthGuard} from './login/auth.guard';
+import { DashboardService } from './dashboard/dashboard.service';
 
 
 @NgModule({
@@ -34,7 +35,7 @@ import {AuthGuard} from './login/auth.guard';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [LoginService, AuthGuard],
+  providers: [LoginService, AuthGuard, DashboardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -5,14 +5,20 @@ import { AngularFireDatabase } from '@angular/fire/database';
 @Injectable()
 export class DashboardService {
   searchHistoryRef: any;
+  firstNamesRef: any; 
   constructor(
     private loginService: LoginService,
     private db: AngularFireDatabase,
     ) {
     this.searchHistoryRef = this.db.list(`currentSession/${this.loginService.userUid}/searches`);
+    this.firstNamesRef = this.db.list('firstNames').valueChanges();
+    this.firstNamesRef.subscribe((fnames: any[]) => {
+      console.log(fnames);
+    })
   }
 
   getSearchHistory() {
     return this.searchHistoryRef.valueChanges();
   }
+
 }
