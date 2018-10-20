4854fba91b89efbaaffee33291a47bcb6c66d761
baumannalexj <baumannalexj@gmail.com>
2018-10-20 16:21:28 -0500

    work following along in class

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 // import {switchMap} from 'rxjs/operators';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/of';
-import { AngularFireDatabase } from '@angular/fire/database';
+import {AngularFireDatabase} from '@angular/fire/database';
+
 // import * as firebase from 'firebase';
 import * as firebase from 'firebase/app';
 
@@ -15,45 +16,48 @@ export class LoginService {
 
   user: Observable<{} | null>;
   userUid: string;
+
   constructor(
-    private afAuth: AngularFireAuth,
+    private angularFireAuth: AngularFireAuth,
     private router: Router,
     private db: AngularFireDatabase
   ) {
 
-    this.user = this.afAuth.authState
-    .switchMap((user) => {
-      if (user) {
-        this.userUid = user.uid;
-        console.log('SWITCHMAP');
-        console.log(user);
-        console.log('SWITCHMAP');
-        return this.db.object(`users/${user.uid}`).update({email: user.email}).then( () => {
-          return this.db.object(`users/${user.uid}`).valueChanges();
-        }).catch( (error) => {
-          console.log('ERROR UPDATING USER EMAIL');
-          console.log(error);
-          console.log('ERROR UPDATING USER EMAIL');
-        });
-      } else {
-        return Observable.of(null);
-      }
-    });
+    this.user = this.angularFireAuth.authState
+      .switchMap((user) => {
+        if (user) {
+          this.userUid = user.uid;
+          console.log('SWITCHMAP');
+          console.log(user);
+          console.log('SWITCHMAP');
+          return this.db.object(`users/${user.uid}`).update({email: user.email}).then(() => {
+            return this.db.object(`users/${user.uid}`).valueChanges();
+          }).catch((error) => {
+            console.log('ERROR UPDATING USER EMAIL');
+            console.log(error);
+            console.log('ERROR UPDATING USER EMAIL');
+          });
+        } else {
+          return Observable.of(null);
+        }
+      });
   }
 
   loginWithEmail(email: string, password: string) {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+    return this.angularFireAuth.auth
+      .signInWithEmailAndPassword(email, password) //angular fire always returns promises
       .then((auth) => {
         console.log(auth.user.uid);
         const createdAt = firebase.database.ServerValue.TIMESTAMP;
         console.log('CREATED AT');
         console.log(createdAt);
         console.log('CREATED AT');
+
         const sessionKey = this.db.database
-                        .ref(`sessions`)
-                        .push({
-                          userUid: auth.user.uid
-                        }).key;
+          .ref(`sessions`)
+          .push({
+            userUid: auth.user.uid
+          }).key;
 
         const sessionPayload: any = {
           createdAt: createdAt,
@@ -64,16 +68,18 @@ export class LoginService {
         const sessionPayloads: any = {};
         sessionPayloads[`currentSession/${auth.user.uid}`] = sessionPayload;
         sessionPayloads[`users/${auth.user.uid}/sessions/${sessionKey}`] = {'createdAt': createdAt};
-        return this.db.database.ref().update(sessionPayloads);
+
+        return this.db.database.ref()
+          .update(sessionPayloads);
       })
       .catch(error => {
-        console.log(error);
+        console.error(error);
         throw error;
       });
   }
 
   signOut() {
-    this.afAuth.auth.signOut();
+    this.angularFireAuth.auth.signOut();
     this.router.navigate(['/']);
   }
 }
