1499f262ec88083d7df4b0da2ed86759888f50b3
Paul Sons <psons@psons@comcast.net>
2018-10-23 12:40:45 -0500

    serach and add name working

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,8 +7,10 @@ import { DashboardService } from './dashboard.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  @Input('inputFirstName') inputFirstName: string;
-  @Input('inputLastName') inputLastName: string;
+  @Input() inputFirstName: string = "";
+  @Input() inputLastName: string = "";
+  firstNameFound: any = false;
+  lastNameFound: any = false;
 
   searches: any[];
   constructor(private dashboardService: DashboardService) {
@@ -16,9 +18,53 @@ export class DashboardComponent implements OnInit {
   }
 
   searchNames() {
-    console.log(`searchNames Called: inputFirstName {inputFirstName},inputLastName {inputLastName} `);
+    console.log(
+      `searchNames Called: `,
+      `inputFirstName ${this.inputFirstName}`,
+      `inputLastName ${this.inputLastName}`
+    );
 
+    if (this.inputFirstName ) {
+      // BEGIN ASYNC
+      this.dashboardService.firstNameSearch(this.inputFirstName).subscribe(n => {
+        this.firstNameFound = n[0];
+        console.log('firstNameFound:' + this.firstNameFound, JSON.stringify(n));
+      });
+      // END ASYNC
+    } else {
+      this.firstNameFound = false;
+    }
+
+    if (this.inputLastName) {
+      // BEGIN ASYNC
+      this.dashboardService.lastNameSearch(this.inputLastName).subscribe(n => {
+        this.lastNameFound = n[0];
+        console.log('lastNameFound:'+ this.lastNameFound, JSON.stringify(n));
+      });
+      // END ASYNC
+    } else {
+      this.lastNameFound = false;
+    }
   }
+
+  unlistedFirstName(){
+    return (!this.firstNameFound && this.inputFirstName);
+  }
+
+  unlistedLastName(){
+    return (!this.lastNameFound && this.inputLastName);
+  }
+
+  addNames() {
+    if (this.unlistedFirstName()) {
+      this.dashboardService.firstNameAdd(this.inputFirstName)
+    }
+    if (this.unlistedLastName()) {
+      console.log('TODO need to add a lastName');
+      this.dashboardService.lastNameAdd(this.inputLastName)
+    }
+  }
+
   searchHistory() {
     this.dashboardService.getSearchHistory().subscribe( (history: any) => {
       this.searches = history;
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -2,27 +2,53 @@ import { Injectable } from '@angular/core';
 import { LoginService } from '../login/login.service';
 import { AngularFireDatabase } from '@angular/fire/database';
 
+
 @Injectable()
 export class DashboardService {
   searchHistoryRef: any;
-  firstNamesRef: any;
+  firstNamesListRef: any;
   constructor(
     private loginService: LoginService,
     private db: AngularFireDatabase,
     ) {
     this.searchHistoryRef = this.db.list(`currentSession/${this.loginService.userUid}/searches`);
-    this.firstNamesRef = this.db.list('firstNames').valueChanges();
-    this.firstNamesRef.subscribe((fnames: any[]) => {
-      console.log("name: " + fnames);
-    });
+    // this.firstNamesListRef = this.db.list('firstNames', ref => ref.orderByKey().equalTo('Kathy')).valueChanges();
+    // this.firstNamesListRef.subscribe((fnames: any[]) => {
+    //   console.log("name: " + fnames);
+    // });
   }
 
-  // searchName(){
-  //   db.list('/items', ref => ref.orderByChild('size').equalTo('large'))
-  // }
+  firstNameAdd(firstName: string){
+    const theList = this.db.list('firstNames');
+    return theList.set(firstName, 'true');
+  }
+
+  lastNameAdd(lastName: string){
+    const theList = this.db.list('lastNames');
+    return theList.set(lastName, 'true');
+  }
+
+
+  firstNameSearch(firstName: string){
+    const theQuery = this.db.list('firstNames',
+      ref => ref.orderByKey().equalTo(firstName));
+    return theQuery.valueChanges();
+  }
+
+  lastNameSearch(lastName: string){
+    const theQuery = this.db.list('lastNames',
+      ref => ref.orderByKey().equalTo(lastName));
+    return theQuery.valueChanges();
+  }
 
   getSearchHistory() {
     return this.searchHistoryRef.valueChanges();
+  }
+
+ debugDir(tag: string, anObject: any) {
+    console.log(`${tag}:${anObject}`);
+    console.dir(anObject);
+    console.log(typeof anObject);
   }
 
 }
