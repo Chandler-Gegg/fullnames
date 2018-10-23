0377e68463502213cee753e6037ab444f014f447
unknown <hanwei.chicago@gmail.com>
2018-10-23 14:20:14 -0500

    finished searching fullname

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,21 @@ import { DashboardService } from './dashboard.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  firstName: string = '';
+  lastName: string = '';
+  disableAddNameBtn: boolean = true;
+  isFirstNameValid: boolean;
+  isLastNameValid: boolean;
+  message: string = null;
+
   searches: any[];
+
   constructor(private dashboardService: DashboardService) {
     this.searches = [];
+  }
+
+  ngOnInit() {
+    this.dashboardService.testGetData();
   }
 
   searchHistory() {
@@ -18,7 +30,45 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-  }
+  searchFullName() {
+    if (this.firstName.length === 0 || this.lastName.length === 0) {
+      return;
+    }
 
+    // check if first name is valid
+    this.dashboardService.searchFirstName(this.firstName).subscribe(
+      (itemFirstName) => {
+        this.isFirstNameValid = (itemFirstName == true ? true : false);
+        console.log(`first name: ${this.firstName}-->${itemFirstName}`);
+        
+        // check if last name is valid
+        this.dashboardService.searchLastName(this.lastName).subscribe(
+          (itemLastName) => {
+            this.isLastNameValid = (itemLastName == true ? true : false);
+            console.log(`last name: ${this.lastName}-->${itemLastName}`);
+
+            // construct the message
+            if (this.isFirstNameValid && this.isLastNameValid) {
+              this.message = `${this.firstName} ${this.lastName} is a valid full name!`;
+            } else if (this.isFirstNameValid) {
+              this.message = `${this.lastName} is not a valid last name!`;
+            } else if (this.isLastNameValid) {
+              this.message = `${this.firstName} is not a valid first name!`;
+            } else {
+              this.message = `${this.firstName} ${this.lastName} is not a valid full name!`;
+            }
+
+            // enable add name button
+            if (this.isFirstNameValid && this.isLastNameValid) {
+              this.disableAddNameBtn = true;
+            } else {
+              this.disableAddNameBtn = false;
+            }
+            
+          }
+        );
+      }
+    );
+    
+  }
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -4,7 +4,12 @@ import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable()
 export class DashboardService {
+
   searchHistoryRef: any;
+  // for test...begin
+  firstNamesRef: any;
+  // for test...end
+
   constructor(
     private loginService: LoginService,
     private db: AngularFireDatabase,
@@ -14,5 +19,25 @@ export class DashboardService {
 
   getSearchHistory() {
     return this.searchHistoryRef.valueChanges();
+  }
+
+  searchFirstName(firstName: string) {
+    return this.db.object(`firstNames/${firstName}`).valueChanges();
+  }
+
+  searchLastName(lastName: string) {
+    return this.db.object(`lastNames/${lastName}`).valueChanges();
+  }
+
+  // for test...begin
+  testGetData() {
+    this.firstNamesRef = this.db.list(`firstNames`).valueChanges().subscribe(
+      (firstNames:any[]) => {
+        console.log('get firstNames...begin');
+        console.log(firstNames);
+        console.log('get firstNames...end');
+      }
+    );
+
   }
 }
